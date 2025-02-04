ca56ea1ab70a029366de58b1aefa64b705691d19
RAPHAEL FERNANDES MOREIRA <raphaelfernandes.moreira@portoseguro.com.br>
2025-02-04 12:25:40 -0300

    fix(cypres.config): add backtrace in blockHosts to fix redirect problem on login.

--- cypress.config.js
+++ cypress.config.js
@@ -1,27 +1,31 @@
-const { defineConfig } = require('cypress')
+const {defineConfig} = require('cypress')
 
 module.exports = defineConfig({
-  //video: false,
-  //screenshotOnRunFailure: false,
-  useTsLoader: true,
+    //video: false,
+    //screenshotOnRunFailure: false,
+    useTsLoader: true,
 
-  viewportWidth: 1280,
-  viewportHeight: 800,
-  e2e: {
-    baseUrl: "https://www.saucedemo.com/",
-  },
-  reporter: 'junit',
-  reporterOptions: {
-    mochaFile: 'tests/test-output-[hash].xml',
-    toConsole: true,
-    attachments: true,
-  },
-  retries: {
-    // Configure retry attempts for `cypress run`
-    // Default is 0
-    runMode: 2,
-    // Configure retry attempts for `cypress open`
-    // Default is 0
-    openMode: 0,
-  },
+    viewportWidth: 1280,
+    viewportHeight: 800,
+    e2e: {
+        specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}",
+        watchForFileChanges: false,
+        chromeWebSecurity: false,
+        blockHosts: ["https://events.backtrace.io"],
+        baseUrl: "https://www.saucedemo.com/",
+    },
+    reporter: 'junit',
+    reporterOptions: {
+        mochaFile: 'tests/test-output-[hash].xml',
+        toConsole: true,
+        attachments: true,
+    },
+    retries: {
+        // Configure retry attempts for `cypress run`
+        // Default is 0
+        runMode: 2,
+        // Configure retry attempts for `cypress open`
+        // Default is 0
+        openMode: 0,
+    },
 })

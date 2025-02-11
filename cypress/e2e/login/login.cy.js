a9e61b176cfda129599354ff75ce82d7c6e5f745
RAPHAEL FERNANDES MOREIRA <raphaelfernandes.moreira@portoseguro.com.br>
2025-02-11 18:17:18 -0300

    feat(login): refactor and add more steps to login test.

--- cypress/e2e/login/login.cy.js
+++ cypress/e2e/login/login.cy.js
@@ -1,22 +1,28 @@
 import LoginPage from "../../page/login-page";
 
-const testCases = [
-    {
-        username: 'standard_user',
-        password: 'secret_sauce'
-    },
-    {
-        username: 'visual_user',
-        password: 'secret_sauce'
-    }
-];
+const testCases = {
+    username: 'standard_user',
+    password: 'secret_sauce'
+}
 
 describe('Test in login', () => {
-    testCases.forEach((testCase, index) => {
-        it(`Test Case ${index + 1}: Login with ${testCase.username}`, () => {
-            cy.visit("")
-            LoginPage.login(testCase.username, testCase.password)
-            LoginPage.verifyLoginSuccess()
-        });
+    beforeEach(() => {
+        cy.visit("")
+    });
+    it(`Test Case 1: Login with ${testCases.username}`, () => {
+        LoginPage.login(testCases.username, testCases.password)
+        LoginPage.verifyLoginSuccess()
+    });
+    it('Test Case 2: Login with invalid username', () => {
+        LoginPage.login('invalid_user', testCases.password)
+        LoginPage.verifyLoginFailed("Epic sadface: Username and password do not match any user in this service")
+    })
+    it('Test Case 3: Login with invalid password', () => {
+        LoginPage.login(testCases.username, 'invalid_password')
+        LoginPage.verifyLoginFailed("Epic sadface: Username and password do not match any user in this service")
+    });
+    it('Test Case 4: Login with empty username and password', () => {
+        LoginPage.login('', '')
+        LoginPage.verifyLoginFailed("Epic sadface: Username is required")
     });
 });
--- cypress/page/login-page.ts
+++ cypress/page/login-page.ts
@@ -1,11 +1,17 @@
 export default class LoginPage {
     static login(username: string, password: string) {
-        cy.get('#user-name').type(username)
-        cy.get('#password').type(password)
+        if (username)
+            cy.get('#user-name').type(username)
+        if (password)
+            cy.get('#password').type(password)
         cy.get('#login-button').click()
     }
 
     static verifyLoginSuccess() {
         cy.get('[data-test="title"]').should("have.text", "Products");
+    }
+
+    static verifyLoginFailed(textError:string) {
+        cy.get('[data-test="error"]').should("have.text", textError);
     }
 }

0325391188b3ef7c1eb2c71594cc961083c9047a
RAPHAEL FERNANDES MOREIRA <raphaelfernandes.moreira@portoseguro.com.br>
2025-02-12 13:39:09 -0300

    fix(inventory): apply DRY in test

--- cypress/e2e/login/inventory.cy.js
+++ cypress/e2e/login/inventory.cy.js
@@ -4,41 +4,29 @@ import InventoryPage from "../../page/inventory-page";
 const cartTestCases = [
     {
         item: "Sauce Labs Backpack",
-        username: "standard_user",
-        password: "secret_sauce",
     },
     {
         item: "Sauce Labs Bike Light",
-        username: "standard_user",
-        password: "secret_sauce",
     },
     {
         item: "Sauce Labs Bolt T-Shirt",
-        username: "standard_user",
-        password: "secret_sauce",
     },
     {
         item: "Sauce Labs Onesie",
-        username: "standard_user",
-        password: "secret_sauce",
     },
     {
         item: "Sauce Labs Fleece Jacket",
-        username: "standard_user",
-        password: "secret_sauce",
     },
     {
         item: "Sauce Labs Onesie",
-        username: "standard_user",
-        password: "secret_sauce",
     },
 ];
 
 describe('Test in inventory', () => {
     cartTestCases.forEach((testCase, index) => {
         it(`Test Case ${index + 1}: Login and add "${testCase.item}" to cart`, () => {
-            cy.visit("", {retryOnStatusCodeFailure: true})
-            LoginPage.login(testCase.username, testCase.password)
+            cy.visit(Cypress.env('BASE_URL'))
+            LoginPage.login(Cypress.env('USERNAME'), Cypress.env('PASSWORD'))
             LoginPage.verifyLoginSuccess()
             InventoryPage.addToCart(testCase.item)
             InventoryPage.verifyItemAddedToCart(testCase.item)
